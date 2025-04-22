0fc9ff1feb2226a33ea4f451878e1ba85083ed71
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-22 21:35:11 +0800

    added the product model

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-const connectDB = async () => {
+export const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB is connected");
@@ -9,4 +9,3 @@ const connectDB = async () => {
     console.error(`Mongodb connected failed`, err);
   }
 };
-export default connectDB;
