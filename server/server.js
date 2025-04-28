9dfce6e1da49364cc887c12c1eb0f68b36daaaac
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-28 21:10:21 +0800

    published the frontend in the vercel and for the backend is in the render

--- server/server.js
+++ server/server.js
@@ -8,10 +8,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const ALLOWED_ORIGINS = [
+  process.env.CLIENT_URL, // https://storeapp-zeta.vercel.app
+  "http://localhost:3000", // for local React
+];
+s;
 //middlewares
 app.use(
   cors({
-    origin: ["https://storeapp-zeta.vercel.app/"],
+    origin: ALLOWED_ORIGINS,
   })
 );
 app.use(express.json()); // allows us to accept json data in the req.body
