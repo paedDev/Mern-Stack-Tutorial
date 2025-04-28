906bb5234c592954da4f9f65ffe344c226ffbc96
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-28 23:25:02 +0800

    still fixing the deployment

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -53,7 +53,7 @@ const HomePage = () => {
                     ) : (
                         <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-1 gap-6'>
                             {
-                                productList.map((productItem) => (<ProductCard
+                                productList.map((productItem) => (<ProductCard key={productItem._id}
                                     productItem={productItem} fetchListsOfProducts={fetchListsOfProducts} />
                                 ))
                             }
--- client/src/utils/apiPaths.js
+++ client/src/utils/apiPaths.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_URL || "https://product-app-store.onrender.com";
